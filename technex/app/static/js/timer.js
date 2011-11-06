7ef7805e36a383d9c8edf7b7fcb246581c8c39b7
Srijan Mishra <srijan23mishra@gmail.com>
2011-11-06 20:53:10 +0530

    Tab border removed, fixed timer

--- technex/app/static/js/timer.js
+++ technex/app/static/js/timer.js
@@ -1,4 +1,4 @@
-dateFuture1 = new Date(2012,2,16,23,59,59); 
+dateFuture1 = new Date(2012,1,13,23,59,59); 
 function GetCount(ddate,iid){ 
  
  dateNow = new Date();
@@ -23,11 +23,11 @@ function GetCount(ddate,iid){
  
   secs=Math.floor(amount);
  
-  if(days != 0){out += days +" "+((days==1)?"D":"D")+", ";} 
-  out += (hours<=9?'0':'')+hours +" "+((hours==1)?"":"")+": "; 
-  out += (mins<=9?'0':'')+mins +" "+((mins==1)?"":"")+": "; 
-  out += (secs<=9?'0':'')+secs +" "+((secs==1)?"":"")+"TECHNEX 2012 COUNTDOWN"; 
-  out = out.substr(0,out.length-0); 
+  if(days != 0){out += days +" "+((days==1)?"D":"Days")+" left";} 
+  //out += (hours<=9?'0':'')+hours +" "+((hours==1)?"":"Hours")+": "; 
+  //out += (mins<=9?'0':'')+mins +" "+((mins==1)?"":"")+": "; 
+  //out += (secs<=9?'0':'')+secs +" "+((secs==1)?"":""); 
+  //out = out.substr(0,out.length); 
   document.getElementById(iid).innerHTML=out; 
  
   setTimeout(function(){GetCount(ddate,iid)}, 1000); 
